5775c9eaea66c6834c8087d376a28d345d7412d3
cirops <cirops@gmail.com>
2023-03-07 20:27:32 -0300

    feat: adds proof of work by difficulty

---
+++ src/entities/Blockchain.js
@@ -0,0 +1,44 @@
+import { Block } from "./Block.js";
+
+export class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 3;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2023", "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (
+        currentBlock.hash !== currentBlock.calculateHash() ||
+        currentBlock.previousHash !== previousBlock.hash
+      ) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  printBlockChain() {
+    for (const block of this.chain) {
+      console.log(block);
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,73 +1,10 @@
-import SHA256 from "crypto-js/sha256.js";
+import { Block } from "./entities/Block.js";
+import { Blockchain } from "./entities/Blockchain.js";
 
-class Block {
-  constructor(index, timestamp, data, previousHash = "") {
-    this.index = index;
-    this.timestamp = timestamp;
-    this.data = data;
-    this.previousHash = previousHash;
-    this.hash = this.calculateHash();
-  }
+const learningCoin = new Blockchain();
 
-  calculateHash() {
-    return SHA256(
-      this.index +
-        this.previousHash +
-        this.timestamp +
-        JSON.stringify(this.data)
-    ).toString();
-  }
-}
-
-class Blockchain {
-  constructor() {
-    this.chain = [this.createGenesisBlock()];
-  }
-
-  createGenesisBlock() {
-    return new Block(0, "01/01/2023", "Genesis Block", "0");
-  }
-
-  getLatestBlock() {
-    return this.chain[this.chain.length - 1];
-  }
-
-  addBlock(newBlock) {
-    newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
-    this.chain.push(newBlock);
-  }
-
-  isChainValid() {
-    for (let i = 1; i < this.chain.length; i++) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
-
-      if (
-        currentBlock.hash !== currentBlock.calculateHash() ||
-        currentBlock.previousHash !== previousBlock.hash
-      ) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  printBlockChain() {
-    for (const block of this.chain) {
-      console.log(block);
-    }
-  }
-}
-
-let learningCoin = new Blockchain();
+console.log("Mining block 1...");
 learningCoin.addBlock(new Block(1, "10/07/2023", { amount: 4 }));
+
+console.log("Mining block 2... ");
 learningCoin.addBlock(new Block(2, "12/07/2023", { amount: 10 }));
-
-console.log(`Is block chain valid? ${learningCoin.isChainValid()}`);
-
-learningCoin.chain[1].data = { amount: 100 };
-learningCoin.chain[1].hash = learningCoin.chain[1].calculateHash();
-
-console.log(`Is block chain valid? ${learningCoin.isChainValid()}`);
